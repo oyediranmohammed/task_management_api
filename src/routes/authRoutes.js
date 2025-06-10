73adf9e7c8f38a77da25e81e0fb05a15d4edcff3
Mohammed Oyediran <95849758+oyediranmohammed@users.noreply.github.com>
2025-06-11 02:14:37 +0300

    Update authRoutes.js

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const authController = require('../controllers/auth.controller');
-const validate = require('../middlewares/validate.middleware');
-const { registerSchema, loginSchema } = require('../validations/auth.validation');
+const authController = require('../controllers/authController');
+const validate = require('../middlewares/validateMiddleware');
+const { registerSchema, loginSchema } = require('../validations/authValidation');
 
 // @route   POST /api/auth/register
 // @desc    Register a new user
